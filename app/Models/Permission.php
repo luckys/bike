c2cfaac034b6036ceb5c09fe844110da7db3f5b0
Dailos <dailos.marrero@hyve.net>
2015-10-26 22:05:27 +0100

    Init commit

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dailosmarrero
+ * Date: 25/10/2015
+ * Time: 13:14
+ */
+
+namespace app\Models;
+
+
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+}
