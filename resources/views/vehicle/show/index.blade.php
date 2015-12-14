206e2b597fdbbe2c4ebb2267bc708ae4081545d4
Dailos <dailos@gmail.com>
2015-12-14 09:31:55 +0100

    Fix view name

--- resources/views/vehicle/show/index.blade.php
+++ resources/views/vehicle/show/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 @section('content')
     <div id="show">
-        @include('vehicle.list._'.$vehicle->getType(), ['vehicle' => $vehicle]);
+        @include('vehicle.show._'.$vehicle->getType(), ['vehicle' => $vehicle]);
     </div>
 @endsection
