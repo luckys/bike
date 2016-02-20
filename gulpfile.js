861ef0b36f66328e010f594f3d49c2dda922948a
Dailos <dailos.marrero@hyve.net>
2016-02-20 10:38:41 +0100

    instalado adminlte2, migración a laravel 5.2

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,13 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss', 'public/css/app.css');
-});
-
-elixir(function (mix) {
-    mix.scripts([
-        'jquery-2.1.4.js',
-        '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-        'app.js'
-    ], 'public/js/app.js');
-});
-
-elixir(function (mix) {
-    mix.version(['css/app.css', 'js/app.js']);
+    mix.less('app.less');
+    mix.less('admin-lte/AdminLTE.less');
+    mix.less('bootstrap/bootstrap.less');
 });
